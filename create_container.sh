193b0b5e3b09273c6f06ffcca0eef641b5627ac4
Derrek Landauer <Derrekito@users.noreply.github.com>
2024-06-06 08:32:50 -0600

    added user prompt for project path when creating a container

--- create_container.sh
+++ create_container.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-LOCAL_DIR="/home/derrekito/Projects/SpaceBench"
+# Prompt the user to provide the path for LOCAL_DIR
+read -p "Please enter the path for LOCAL_DIR: " LOCAL_DIR
+
+# Check if LOCAL_DIR is provided
+if [ -z "$LOCAL_DIR" ]; then
+    echo "Error: LOCAL_DIR path is required."
+    exit 1
+fi
+
 CONTAINER_DIR="/shared"
 
 IMAGE_TAG="cudatools:11.8"
